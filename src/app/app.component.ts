cfbb7a2225742ed915a636089814772eec036ad4
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-06 18:53:16 -0700

    feat: example of reactive and template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'forms-project';
+  favoriteColorControl = new FormControl('');
+  favoriteColor = '';
 }
