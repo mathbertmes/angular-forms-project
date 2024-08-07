f43e9ae80c3dda0e9b6048472c2316a22d52cfb7
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-06 20:04:34 -0700

    feat: imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SimpleFormComponent } from "./components/reactive/simple-form/simple-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, FormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule, SimpleFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
