9b042a812711ff8d9e12e9554d5223c483714b7e
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-06 20:04:07 -0700

    feat: form control and inputs

--- src/app/components/reactive/simple-form/simple-form.component.ts
+++ src/app/components/reactive/simple-form/simple-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './simple-form.component.html',
   styleUrl: './simple-form.component.css'
 })
 export class SimpleFormComponent {
-
+  name = new FormControl('');
+  updateName() { this.name.setValue('Mikel'); }
 }
